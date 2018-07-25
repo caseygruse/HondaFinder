78e0045db0473e4831494b30e7397a1cf132c3ef
caseygruse <36206092+caseygruse@users.noreply.github.com>
2018-07-25 12:48:44 -0700

    added all options to add vehical form combo boxes. the text boxes still need validation

--- HondaFinder-master/HondaFinder/frmAddHonda.Designer.cs
+++ HondaFinder-master/HondaFinder/frmAddHonda.Designer.cs
@@ -28,164 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnExit = new System.Windows.Forms.Button();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.txtColor = new System.Windows.Forms.TextBox();
-            this.cmbPrice = new System.Windows.Forms.ComboBox();
-            this.cmbCondition = new System.Windows.Forms.ComboBox();
-            this.cmbMileage = new System.Windows.Forms.ComboBox();
-            this.txtYear = new System.Windows.Forms.TextBox();
-            this.cmbModel = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(160, 262);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(95, 59);
-            this.btnExit.TabIndex = 20;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(29, 262);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(95, 59);
-            this.btnSearch.TabIndex = 21;
-            this.btnSearch.Text = "Add Honda";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            // 
-            // txtColor
-            // 
-            this.txtColor.Location = new System.Drawing.Point(89, 206);
-            this.txtColor.Name = "txtColor";
-            this.txtColor.Size = new System.Drawing.Size(121, 20);
-            this.txtColor.TabIndex = 19;
-            // 
-            // cmbPrice
-            // 
-            this.cmbPrice.FormattingEnabled = true;
-            this.cmbPrice.Location = new System.Drawing.Point(89, 164);
-            this.cmbPrice.Name = "cmbPrice";
-            this.cmbPrice.Size = new System.Drawing.Size(121, 21);
-            this.cmbPrice.TabIndex = 18;
-            // 
-            // cmbCondition
-            // 
-            this.cmbCondition.FormattingEnabled = true;
-            this.cmbCondition.Location = new System.Drawing.Point(89, 125);
-            this.cmbCondition.Name = "cmbCondition";
-            this.cmbCondition.Size = new System.Drawing.Size(121, 21);
-            this.cmbCondition.TabIndex = 17;
-            // 
-            // cmbMileage
-            // 
-            this.cmbMileage.FormattingEnabled = true;
-            this.cmbMileage.Location = new System.Drawing.Point(89, 89);
-            this.cmbMileage.Name = "cmbMileage";
-            this.cmbMileage.Size = new System.Drawing.Size(121, 21);
-            this.cmbMileage.TabIndex = 16;
-            // 
-            // txtYear
-            // 
-            this.txtYear.Location = new System.Drawing.Point(89, 54);
-            this.txtYear.Name = "txtYear";
-            this.txtYear.Size = new System.Drawing.Size(121, 20);
-            this.txtYear.TabIndex = 15;
-            // 
-            // cmbModel
-            // 
-            this.cmbModel.FormattingEnabled = true;
-            this.cmbModel.Location = new System.Drawing.Point(89, 21);
-            this.cmbModel.Name = "cmbModel";
-            this.cmbModel.Size = new System.Drawing.Size(121, 21);
-            this.cmbModel.TabIndex = 14;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(24, 209);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 13);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Color:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 167);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Price:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 128);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Condition:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 92);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Mileage:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Year:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Model:";
-            // 
-            // frmAddHonda
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 350);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnSearch);
-            this.Controls.Add(this.txtColor);
-            this.Controls.Add(this.cmbPrice);
-            this.Controls.Add(this.cmbCondition);
-            this.Controls.Add(this.cmbMileage);
-            this.Controls.Add(this.txtYear);
-            this.Controls.Add(this.cmbModel);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "frmAddHonda";
-            this.Text = "Add a Honda";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnSearch = new System.Windows.Forms.Button();
+			this.txtColor = new System.Windows.Forms.TextBox();
+			this.cmbCondition = new System.Windows.Forms.ComboBox();
+			this.cmbModel = new System.Windows.Forms.ComboBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtMileage = new System.Windows.Forms.TextBox();
+			this.cmbYear = new System.Windows.Forms.ComboBox();
+			this.txtPrice = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(160, 262);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(95, 59);
+			this.btnExit.TabIndex = 20;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnSearch
+			// 
+			this.btnSearch.Location = new System.Drawing.Point(29, 262);
+			this.btnSearch.Name = "btnSearch";
+			this.btnSearch.Size = new System.Drawing.Size(95, 59);
+			this.btnSearch.TabIndex = 21;
+			this.btnSearch.Text = "Add Honda";
+			this.btnSearch.UseVisualStyleBackColor = true;
+			// 
+			// txtColor
+			// 
+			this.txtColor.Location = new System.Drawing.Point(89, 206);
+			this.txtColor.Name = "txtColor";
+			this.txtColor.Size = new System.Drawing.Size(121, 20);
+			this.txtColor.TabIndex = 19;
+			// 
+			// cmbCondition
+			// 
+			this.cmbCondition.FormattingEnabled = true;
+			this.cmbCondition.Location = new System.Drawing.Point(89, 125);
+			this.cmbCondition.Name = "cmbCondition";
+			this.cmbCondition.Size = new System.Drawing.Size(121, 21);
+			this.cmbCondition.TabIndex = 17;
+			// 
+			// cmbModel
+			// 
+			this.cmbModel.FormattingEnabled = true;
+			this.cmbModel.Location = new System.Drawing.Point(89, 21);
+			this.cmbModel.Name = "cmbModel";
+			this.cmbModel.Size = new System.Drawing.Size(121, 21);
+			this.cmbModel.TabIndex = 14;
+			this.cmbModel.SelectedIndexChanged += new System.EventHandler(this.cmbModel_SelectedIndexChanged);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(24, 209);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(34, 13);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Color:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(24, 167);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(34, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Price:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(24, 128);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(54, 13);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Condition:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(24, 92);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(47, 13);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Mileage:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(24, 57);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 13);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Year:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(24, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(39, 13);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "Model:";
+			// 
+			// txtMileage
+			// 
+			this.txtMileage.Location = new System.Drawing.Point(89, 92);
+			this.txtMileage.Name = "txtMileage";
+			this.txtMileage.Size = new System.Drawing.Size(121, 20);
+			this.txtMileage.TabIndex = 22;
+			// 
+			// cmbYear
+			// 
+			this.cmbYear.FormattingEnabled = true;
+			this.cmbYear.Location = new System.Drawing.Point(89, 57);
+			this.cmbYear.Name = "cmbYear";
+			this.cmbYear.Size = new System.Drawing.Size(121, 21);
+			this.cmbYear.TabIndex = 23;
+			// 
+			// txtPrice
+			// 
+			this.txtPrice.Location = new System.Drawing.Point(89, 167);
+			this.txtPrice.Name = "txtPrice";
+			this.txtPrice.Size = new System.Drawing.Size(121, 20);
+			this.txtPrice.TabIndex = 24;
+			// 
+			// frmAddHonda
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(282, 350);
+			this.Controls.Add(this.txtPrice);
+			this.Controls.Add(this.cmbYear);
+			this.Controls.Add(this.txtMileage);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnSearch);
+			this.Controls.Add(this.txtColor);
+			this.Controls.Add(this.cmbCondition);
+			this.Controls.Add(this.cmbModel);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "frmAddHonda";
+			this.Text = "Add a Honda";
+			this.Load += new System.EventHandler(this.frmAddHonda_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -194,10 +195,7 @@
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtColor;
-        private System.Windows.Forms.ComboBox cmbPrice;
         private System.Windows.Forms.ComboBox cmbCondition;
-        private System.Windows.Forms.ComboBox cmbMileage;
-        private System.Windows.Forms.TextBox txtYear;
         private System.Windows.Forms.ComboBox cmbModel;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
@@ -205,5 +203,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.TextBox txtMileage;
+		private System.Windows.Forms.ComboBox cmbYear;
+		private System.Windows.Forms.TextBox txtPrice;
+	}
 }
--- HondaFinder-master/HondaFinder/frmAddHonda.cs
+++ HondaFinder-master/HondaFinder/frmAddHonda.cs
@@ -21,5 +21,64 @@ namespace HondaFinder
         {
             this.Close();
         }
-    }
+
+		private void cmbModel_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void frmAddHonda_Load(object sender, EventArgs e)
+		{
+			LoadVehicalModels();
+			AddVehicalYears();
+			AddVehicalConditions();
+		}
+
+		/// <summary>
+		/// loads form data for addVehical Form
+		/// </summary>
+		private void LoadVehicalModels()
+		{
+			//add models to comboBox
+			cmbModel.Items.Add("Civic");
+			cmbModel.Items.Add("Accord");
+			cmbModel.Items.Add("Insight");
+			cmbModel.Items.Add("Accord");
+			cmbModel.Items.Add("Clarity");
+			cmbModel.Items.Add("Fit");
+			cmbModel.Items.Add("HR-V");
+			cmbModel.Items.Add("CR-V");
+			cmbModel.Items.Add("Pilot");
+			cmbModel.Items.Add("Odyssey");
+			cmbModel.Items.Add("Ridgeline");
+			cmbModel.Items.Add("Accord Hybrid");
+			cmbModel.Items.Add("Clarity Hybrid");
+			cmbModel.Items.Add("Prelude");
+		}
+
+		/// <summary>
+		/// adds years to comboBox
+		/// </summary>
+		private void AddVehicalYears()
+		{
+			for(int i = 1985; i <= 2018; i++)
+			{
+				cmbYear.Items.Add(i);
+			}
+		}
+
+		/// <summary>
+		/// adds vehicalConditions to comboBox
+		/// </summary>
+		private void AddVehicalConditions()
+		{
+
+			cmbCondition.Items.Add("Outstanding");
+			cmbCondition.Items.Add("Clean");
+			cmbCondition.Items.Add("Average");
+			cmbCondition.Items.Add("Rough");
+			cmbCondition.Items.Add("Damaged");
+
+		}
+	}
 }
