fdabd65489f83f1d07abf0f63c03fc82c0fe1dba
caseygruse <36206092+caseygruse@users.noreply.github.com>
2018-07-27 12:53:48 -0700

    Customers can now be added to the data base. There is whitespace validation but more is needed

---
+++ HondaFinder-master/HondaFinder/CustomerDB.cs
@@ -0,0 +1,22 @@
+﻿using HondaFinder.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HondaFinder
+{
+	public static class CustomerDB
+	{
+		public static void AddCustomer(Customer c)
+		{
+			//create db
+			HondaDBContext context = new HondaDBContext();
+			//add customer c to database
+			context.Customers.Add(c);
+			//save changes
+			context.SaveChanges();
+		}
+	}
+}
--- HondaFinder-master/HondaFinder/frmCreateAccount.cs
+++ HondaFinder-master/HondaFinder/frmCreateAccount.cs
@@ -21,5 +21,61 @@ namespace HondaFinder
         {
             this.Close();
         }
-    }
+
+		private void frmCreateAccount_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private bool CheckForWhiteSpace(TextBox box)
+		{
+			if (box.Text.Trim() == "")
+			{
+				//add error
+				return true;
+			}
+			return false;
+		}
+
+		private bool CheckAllTextBoxesForInput()
+		{
+			foreach(Control c in Controls)
+			{
+				if(c is TextBox)
+				{
+					if (CheckForWhiteSpace(c as TextBox))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+
+		private void btnCreateAccount_Click(object sender, EventArgs e)
+		{
+			if (CheckAllTextBoxesForInput())
+			{
+				//create customer object using the textboxes
+				Customer c = new Customer();
+				//add values to properties
+				c.FirstName = txtFirstName.Text;
+				c.LastName = txtLastName.Text;
+				c.PhoneNumber = txtPhoneNumber.Text;
+				c.EmailAddress = txtEmail.Text;
+				c.StreetAddress = txtStreetAddress.Text;
+				c.City = txtCity.Text;
+				c.State = txtState.Text;
+				//validate for zip which is an int.
+				c.ZipCode = Convert.ToInt32(txtZip.Text);
+				CustomerDB.AddCustomer(c);
+				MessageBox.Show("Customer Added");
+			}
+			else
+			{
+				MessageBox.Show("not a fields were filled");
+			}
+		}
+	}
 }
