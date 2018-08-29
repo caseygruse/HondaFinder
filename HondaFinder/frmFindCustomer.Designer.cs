ec27832ec4c28db560d7f76ef003e783112d8c27
eoganz <jakeemettler@gmail.com>
2018-08-29 10:35:04 -0700

    Created find customer functionality using their email address they signed up with.

---
+++ HondaFinder/frmFindCustomer.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace HondaFinder
+{
+    partial class frmFindCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnFindCustomer = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-mail Address:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(108, 19);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(185, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // btnFindCustomer
+            // 
+            this.btnFindCustomer.Location = new System.Drawing.Point(26, 76);
+            this.btnFindCustomer.Name = "btnFindCustomer";
+            this.btnFindCustomer.Size = new System.Drawing.Size(104, 48);
+            this.btnFindCustomer.TabIndex = 2;
+            this.btnFindCustomer.Text = "Find Customer";
+            this.btnFindCustomer.UseVisualStyleBackColor = true;
+            this.btnFindCustomer.Click += new System.EventHandler(this.btnFindCustomer_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(189, 76);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(104, 48);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmFindCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 136);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFindCustomer);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFindCustomer";
+            this.Text = "frmFindCustomer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnFindCustomer;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ HondaFinder/frmFindCustomer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HondaFinder
+{
+    public partial class frmFindCustomer : Form
+    {
+        public frmFindCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFindCustomer_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+
+            if (!String.IsNullOrEmpty(email))
+            {
+                List<Customer> customers = CustomerDB.GetAllCustomers();
+
+                foreach(Customer c in customers)
+                {
+                    if(email == c.EmailAddress)
+                    {
+                        MessageBox.Show("You're customer id is: " + c.CustomerID);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
